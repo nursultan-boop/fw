98eb915c7578331d2e79d78fc8331384c1de3d47
nursultan-boop <60815706+nursultan-boop@users.noreply.github.com>
2024-05-20 14:31:44 +0500

    fuck gpt, im doing it myself

--- admin_panel/app.py
+++ admin_panel/app.py
@@ -26,17 +26,18 @@ def save_data(groups):
 
 def scan_devices():
     """Scan for connected devices using nmcli."""
-    command = "nmcli -t -f DEVICE,IP4.ADDRESS device show"
+    command = "nmcli -t -f DEVICE,IP4.ADDRESS"
     result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     output = result.stdout.decode()
     devices = []
     for line in output.split('\n'):
         if line:
-            parts = line.split(':')
-            if len(parts) == 2 and parts[1]:
-                device = parts[0]
-                ip = parts[1].split('/')[0]
-                devices.append({"name": device, "ip": ip})
+            if "enp" in line:
+                device = line
+            elif "inet4" in line:
+                ip=line.split(' ')[1]
+            devices.append({"name": device, "ip": ip})
+            
     return devices
 
 @app.route('/')
